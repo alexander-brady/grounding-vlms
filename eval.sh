672b454a3d8ec2c89cfed471eae4224391d23e4b
Alexander Brady <129788423+alexander-brady@users.noreply.github.com>
2025-08-02 17:18:55 +0200

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -8,19 +8,8 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --account=es_sachan
 #SBATCH --mail-type=END,FAIL
 #SBATCH --gres=gpumem:64g
-
-# for 12 gemma -> gpu 32 cpu 64 batch 1 worked -> 16 with batchsize 4 didnt work -> 32g batch 4 tbd 32 cpu didnt work ->32 with fast mode batch 1
-
-# MODEL=${1:-"huggingface/Qwen2-5-VL-3B-Instruct"}
-# MODEL=${1:-"huggingface/Qwen2-5-VL-7B-Instruct"}
-# MODEL=${1:-"huggingface/Qwen2-5-VL-72B-Instruct"}
-
-# MODEL=${1:-"huggingface/gemma-3-4b-it"}
-# MODEL=${1:-"huggingface/gemma-3-12b-it"}
-MODEL=${1:-"huggingface/gemma-3-27b-it"}
 
 # MODEL=${1:-"huggingface/Llama-4-Scout-17B-16E-Instruct"}
 
@@ -31,12 +20,6 @@ echo "Job started at $(date)"
 module load stack/2024-06 gcc/12.2.0 python/3.11.6 cuda/11.3.1 eth_proxy
 
 VENV_PATH="$SCRATCH/pmlr/$MODEL/.venv"
-
-# # Delete the venv
-if [ -d "$VENV_PATH" ]; then
-  rm -rf "$VENV_PATH"
-  echo "Cleared existing virtual environment at $VENV_PATH at $(date)"
-fi
 
 # Check if venv exists, create if not
 if [ ! -d "$VENV_PATH" ]; then
